9b9051e9132b2545887992db6785c5e5e3684edb
s-pool <67300011+s-pool@users.noreply.github.com>
2020-08-18 07:14:37 +0000

    Deploying to master from  @ 419c374c8950ec3c563873ebe9c6de5e6b30b0ae 🚀

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3975bad3403758df0486dd27d46da279.js"
+  "./precache-manifest.69e02f2032b9d3c3aca27d569523d8ac.js"
 );
 
 self.addEventListener('message', (event) => {
