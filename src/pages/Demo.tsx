2455e7c10cf1c3e27f9a7ad226cb026f4a6aa6ad
s-pool <67300011+s-pool@users.noreply.github.com>
2020-08-20 00:36:53 +0900

    Update Demo.tsx

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,35 +1,32 @@
 import { teal } from '@material-ui/core/colors'
 import React from 'react'
-import MetaTags from 'react-meta-tags'
 
 import { Logo } from '../assets/static/synapse_logo'
 import { PR } from '../organisms'
 import { SynapseAppBar } from '../pages'
 
 const Demo = () => {
+  React.useEffect(() => {
+    document.title = 'エスプール Synapse - データ分析プラットフォーム'
+  }, [])
+
   return (
-    <React.Fragment>
-      <MetaTags>
-        <title>Synapse | Demo</title>
-        <meta name='description' content='Synapseのデモページ。' />
-      </MetaTags>
-      <PR
-        appbar={{
-          logo: <Logo/>,
-          fontcolor: teal[500],
-          tools: <SynapseAppBar/>
-        }}
-        contents={
-          [
-            {
-              subtitle: 'Coming Soon ...',
-              body: '',
-              split: [false, 12]
-            }
-          ]
-        }
-      />
-    </React.Fragment>
+    <PR
+      appbar={{
+        logo: <Logo/>,
+        fontcolor: teal[500],
+        tools: <SynapseAppBar/>
+      }}
+      contents={
+        [
+          {
+            subtitle: 'Coming Soon ...',
+            body: '',
+            split: [false, 12]
+          }
+        ]
+      }
+    />
   )
 }
 
