62df6171c3906b5867daf2b439d0779df17acb58
ghost <ghost@users.noreply.github.com>
2020-08-19 08:16:53 +0000

    gitmask.com anonymous commit

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,9 +1,9 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const Home = lazy(() => import('../wrap/LazyHome'))
-const FAQEC = lazy(() => import('../wrap/LazyFAQEC'))
-const Demo = lazy(() => import('../wrap/LazyDemo'))
+const Home = lazy(() => import('../wrap/pages/LazyHome'))
+const FAQEC = lazy(() => import('../wrap/pages/LazyFAQEC'))
+const Demo = lazy(() => import('../wrap/pages/LazyDemo'))
 
 const base = (process.env.NODE_ENV !== 'development') ? window.location.pathname.split('/').slice(1, 2)[0] : '.'
 
